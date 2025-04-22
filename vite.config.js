ac09b10ebe9187ebbe86f5f4b6f5213f50f162f2
tnduya3 <23520394@gm.uit.edu.vn>
2025-04-22 18:24:07 +0700

    Fix GitHub Pages deployment issues

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './', // This ensures assets are referenced correctly when deployed
+  base: '/new_react_app/', // Đường dẫn repository của bạn trên GitHub Pages
 })
